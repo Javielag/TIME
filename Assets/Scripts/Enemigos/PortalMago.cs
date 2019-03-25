f85f7b3d804f17059204482586b8f2ddaefb89ec
AitorGarcia <aitgar03@ucm.es>
2019-03-25 12:25:31 +0100

    No detecta al enemigo :(
    He añadido un Debug.Log de paso

--- Assets/Scripts/Enemigos/PortalMago.cs
+++ Assets/Scripts/Enemigos/PortalMago.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PortalMago : MonoBehaviour {
-    bool active = false;
+    [SerializeField]bool active = false;
     Vector2 distance;
 
     private void OnTriggerStay2D(Collider2D collision)
-    {
+    {      
         if (active)
         {
+            Debug.Log(collision.gameObject.name);
             distance = new Vector2(transform.position.x - collision.transform.position.x, transform.position.y - transform.position.y);
             collision.transform.position = new Vector2();
         }
