a0bb89b6169e91aa9b8bae3b5265d2b1a605ced4
AitorGarcia <aitgar03@ucm.es>
2019-03-27 09:30:55 +0100

    Testing
    yeee

--- Assets/Scripts/Enemigos/PortalMago.cs
+++ Assets/Scripts/Enemigos/PortalMago.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PortalMago : MonoBehaviour {
-    [SerializeField]bool active = false, hasTeleported = false;
+    [SerializeField] bool active = false;
     Vector2 distance;
     Vector2 otherPortal;
-
+    [SerializeField]int counter, numberOfTeleports = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        counter++;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        counter--;
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {   //Si está activo y no ha realizado el teletransporte
-        if (active && !hasTeleported)
+        if (active && numberOfTeleports < counter)
         {
-            Debug.Log(collision.gameObject.name);
+            Debug.Log(collision.gameObject.name); Debug.Log(counter + "counter"); Debug.Log(numberOfTeleports + "teleportos");
             distance = new Vector2(transform.position.x - collision.transform.position.x, transform.position.y - collision.transform.position.y);   //Calcula la distancia con el personaje
-            collision.transform.position = new Vector2(otherPortal.x - distance.x, otherPortal.y - distance.y);                                     //Lo pone en la posición correspondiente del otro portal
-            hasTeleported = true;
-            
+            collision.transform.position = new Vector2(otherPortal.x - distance.x, otherPortal.y - distance.y);                                     //Lo pone en la posición correspondiente del otro portal        
+            numberOfTeleports++;
         }
     }
     public void SetOtherPortal(Vector2 thisOne)
