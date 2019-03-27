fb23d1e712b919788620c8ea808f620a09df5b1d
dagodoy <dagodoy@ucm.es>
2019-03-27 10:56:49 +0100

    Pathfinding casi acabado
    Hecho script Pathfinder.cs y Waypoint.cs
    Falta asignar a cada waypoint sus valores

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+    public Transform[] closeWaypoints = new Transform[4];
+    public int sala;
+}
