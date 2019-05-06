a1ea5564678c9aed60c4ef2a419aff3d908cbc2c
David Godoy <dagodoy@ucm.es>
2019-05-06 01:01:24 +0100

    Tenemos hub
    Está el científico y un campo de tiro, falta que se reseteen las armas y poder hablar con el

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour {
+    GameObject player;
+    public string GameScene = "CheatsGuerrilla";
+    private void Start()
+    {
+        player = GameManager.instance.GetPlayer();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            SceneManager.LoadScene(GameScene);
+        }
+    }
+}
