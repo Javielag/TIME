5fdf517a2e859142d50f5f4c7ea2f2f3dc9fd2d0
evalucas <evalucas@ucm.es>
2019-05-10 12:49:01 +0200

    PickUp
    Animaciones para los pickups

--- Assets/Scripts/Armas/WeaponPickup.cs
+++ Assets/Scripts/Armas/WeaponPickup.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WeaponPickup : MonoBehaviour {
 
     public Weapon thisWeapon;
+    public Animator anim;
+
     public Weapon WhatWeapon()
     {
         return thisWeapon;
@@ -12,10 +14,12 @@ public class WeaponPickup : MonoBehaviour {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         transform.GetChild(0).gameObject.SetActive(true);
+        anim.SetBool("pickup", true);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         transform.GetChild(0).gameObject.SetActive(false);
+        anim.SetBool("pickup", false);
         //transform.Find("PickIcon").gameObject.SetActive(false);
     }
     public void OnPicked()
