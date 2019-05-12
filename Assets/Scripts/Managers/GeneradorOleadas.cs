81193acd0e46d3da8bdedc39becf87bef8004dc0
Adrian <adralv01@ucm.es>
2019-05-12 21:16:49 +0200

    Modo testing para el generador de oleadas
    Genera 5 mele, 5 ranged y 5 élites no aleatorios

--- Assets/Scripts/Managers/GeneradorOleadas.cs
+++ Assets/Scripts/Managers/GeneradorOleadas.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GeneradorOleadas : MonoBehaviour { 
 
     public SpawnManager spawnManager;
+    public bool TESTING;
     private void Start()
     {
         GameManager.instance.SetGeneraOleadas(this);
@@ -13,46 +14,63 @@ public class GeneradorOleadas : MonoBehaviour {
     }
     public void GeneraOleada(int ol, out int enemyCount)
     {
-        int a, b; //Variables auxiliares para números aleatorios
-        int[] cants = new int[9];
+       
+        int[] cants = new int[9]; 
         enemyCount = 0;
-        //ol = 1;
-        cants[0] = (int)Mathf.Pow(1.2f, ol) + 4;
-        if (ol >= 5)
+        if (!TESTING)
         {
-            cants[1] = (int)(Mathf.Pow(1.2f, ol) + 4) / 2;  //A partir de la oleada 5 genera enemigos a rango
-            if (ol >= 10)   //A partir de la oleada 10 genera enemigos élite
+            int a, b; //Variables auxiliares para números aleatorios
+            cants = new int[9]; //mel,ranged,caballo,portal,barrera,invbarrera,mago,kamikaze,worm
+            enemyCount = 0;
+            //ol = 1;
+            cants[0] = (int)Mathf.Pow(1.2f, ol) + 4;
+            if (ol >= 5)
             {
-                a = Random.Range(2, cants.Length);
-                cants[a] = (int)(Mathf.Pow(1.2f, ol) + 4) / 8;
-                do
-                    b = Random.Range(2, cants.Length);
-                while (b == a);
-                cants[b] = (int)(Mathf.Pow(1.2f, ol) + 4) / 8;
+                cants[1] = (int)(Mathf.Pow(1.2f, ol) + 4) / 2;  //A partir de la oleada 5 genera enemigos a rango
+                if (ol >= 10)   //A partir de la oleada 10 genera enemigos élite
+                {
+                    a = Random.Range(2, cants.Length);
+                    cants[a] = (int)(Mathf.Pow(1.2f, ol) + 4) / 8;
+                    do
+                        b = Random.Range(2, cants.Length);
+                    while (b == a);
+                    cants[b] = (int)(Mathf.Pow(1.2f, ol) + 4) / 8;
+                }
+
             }
-                
+            else
+            {
+                cants[1] = 0;
+                cants[2] = 0;
+            }
+
+            if (ol % 3 == 2) //Genera portales cada 3 oleadas
+                cants[3] = (ol + 1) / 6;
+            else cants[3] = 0;
+
+            for (int i = 0; i < cants.Length; i++)
+            {
+                enemyCount += cants[i];
+            }
+
+            int x = 0;
+            Debug.Log("OLEADA " + ol);
+            for (int i = 0; i < cants.Length; i++)
+            {
+                Debug.Log(i + " " + cants[i]);
+            }
         }
         else
         {
-            cants[1] = 0;
-            cants[2] = 0;
-        }
-
-        if (ol % 3 == 2) //Genera portales cada 3 oleadas
-            cants[3] = (ol + 1) / 6;
-        else cants[3] = 0;
-
-        for (int i = 0; i<cants.Length;i++)
-        {
-            enemyCount += cants[i];
+            //cada oleada tendrá
+            //5 melées 5 enemigos a rango
+            //y 5 elites
+            cants[0] = 5;
+            cants[1] = 5;
+            cants[ol+1] = 5;
+            for(int i = 0; i < cants.Length; i++) { enemyCount += cants[i]; }
         }
         spawnManager.Enemigos(cants);
-        int x = 0;
-        Debug.Log("OLEADA " +ol);
-        for(int i = 0; i < cants.Length; i++)
-        {
-            Debug.Log(i + " " + cants[i]);
-        }
     }
     public void AcabaOleada()
     {
