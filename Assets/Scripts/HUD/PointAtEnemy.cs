a90559e5a0559c040f901954dfd25b58af810b1d
David Godoy Ruiz <dagodoy@ucm.es>
2019-05-16 16:00:12 +0200

    Flechas

---
+++ Assets/Scripts/HUD/PointAtEnemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtEnemy : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public Transform Target;
+    public Camera camera;
+    private void Update()
+    {
+        Vector2 target = Target.position;
+        Vector2 dir = new Vector2(target.x - transform.position.x, target.y - transform.position.y);
+        PointAt(dir);
+        
+        transform.position = new Vector2(Mathf.Clamp(target.x, target.x - camera.pixelWidth/2, target.x + camera.pixelWidth / 2), Mathf.Clamp(target.y, target.y - camera.pixelWidth / 2, target.y + camera.pixelWidth / 2));
+    }
+    private void PointAt(Vector2 newDir)
+    {
+        transform.up = -newDir;
+    }
+}
---
+++ Assets/Scripts/HUD/PointAtPortal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAtPortal : MonoBehaviour {
+
+	// Use this for initialization
+    private void Start()
+    {
+    }
+    public Transform Target;
+    private void Update()
+    {
+        Vector2 target = Target.position;
+        Vector2 dir = new Vector2(target.x - transform.position.x, target.y - transform.position.y);
+        PointAt(dir);       
+    }
+    private void PointAt(Vector2 newDir)
+    {
+        transform.up = -newDir;
+    }
+}
